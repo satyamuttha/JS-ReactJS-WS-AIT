095c371e0f285f71bd66b8a841fd21ca98248662
Kabir <sagithedeveloper@gmail.com>
2024-04-15 09:03:30 +0530

    added the code

---
+++ S08_15-04-2024/Obj-fun.js
@@ -0,0 +1,13 @@
+var obj = {
+  id: 101,
+  name: "xyz",
+  fn: function (x) {
+    var a = 100;
+    var b = 200;
+    console.log(a + b);
+    console.log(x);
+  },
+};
+
+obj.fn(100);
+console.log(obj);
