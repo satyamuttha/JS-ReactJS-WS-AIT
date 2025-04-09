28c583ad0871a82ac69ed2fefd1fc470d44d9ac7
Kabir <sagithedeveloper@gmail.com>
2025-04-09 09:20:13 +0530

    Added the code

---
+++ S17/Hoisting.js
@@ -0,0 +1,34 @@
+// var x = 10;
+
+// var y = 20;
+
+// console.log(x, y);
+// function f1() {
+//   console.log("f1 is called");
+// }
+
+// f1();
+
+//=================JS Engine Does this =============================
+// var x = undefined;
+// var y = undefined;
+
+// let x;
+//let y;
+
+// function f1() {
+//   console.log("f1 is called");
+// }
+//=================================================
+console.log(x, y);
+
+var x = 10;
+
+var y = 20;
+console.log(x, y);
+
+f1();
+
+function f1() {
+  console.log("f1 is called");
+}
